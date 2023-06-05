723c56ddc8209b697c04a7852cd6408b6fdf60fc
sairex <biggdadddy1999@gmail.com>
2023-06-06 02:26:46 +0700

    Add autocomplete

--- components/common/autocomplete/Autocomplete.vue
+++ components/common/autocomplete/Autocomplete.vue
@@ -1,19 +1,90 @@
 <script>
+import Icon from "../icon/Icon.vue";
+
 export default {
   name: "Autocomplete",
+  props: {
+    options: { type: Array, required: true },
+  },
+  data(props) {
+    const currentOptions = props.options;
+    const value = "";
+    const isOpen = false;
+    const results = [];
+
+    return {
+      results,
+      value,
+      isOpen,
+      currentOptions,
+    };
+  },
+  methods: {
+    open() {
+      this.isOpen = true;
+    },
+    close() {
+      this.isOpen = false;
+    },
+    filterResults() {
+      this.results = this.currentOptions.filter((item) => {
+        return item.name.toLowerCase().indexOf(this.value.toLowerCase()) > -1;
+      });
+    },
+    onChange() {
+      this.filterResults();
+    },
+  },
+  components: { Icon },
 };
 </script>
 
 <template>
   <div class="autocomplete">
-    <input placeholder="Найти услугу" class="autocomplete__input" type="text" />
-    <ul class="autocomplete__options">
-      <!-- <li class="autocomplete__options-item">azaza</li> -->
+    <input
+      placeholder="Найти услугу"
+      class="autocomplete__input"
+      type="text"
+      @input="onChange"
+      @focus="open"
+      @blur="close"
+      v-model="value"
+    />
+    <ul
+      :class="[
+        'autocomplete__options',
+        { ['autocomplete__options_active']: isOpen },
+      ]"
+    >
+      <li
+        v-if="!results"
+        v-for="item in currentOptions"
+        class="autocomplete__options-item"
+      >
+        <Icon name="arrow-right-test" class="autocomplete__options-item-icon" />
+        <span class="autocomplete__options-item-text">{{ item.name }}</span>
+        <NuxtLink :to="item.path" class="autocomplete__options-item-link" />
+      </li>
+      <li
+        v-if="results"
+        v-for="item in results"
+        class="autocomplete__options-item"
+      >
+        <Icon name="arrow-right-test" class="autocomplete__options-item-icon" />
+        <span class="autocomplete__options-item-text">{{ item.name }}</span>
+        <NuxtLink :to="item.path" class="autocomplete__options-item-link" />
+      </li>
+      <li v-if="results.length === 0" class="autocomplete__options-item">
+        <span class="autocomplete__options-item-text">ничего не найдено</span>
+      </li>
     </ul>
   </div>
 </template>
 
 <style lang="scss" scoped>
+@use "@/assets/scss/styles/mixins" as mx;
+@use "@/assets/scss/styles/variables" as vars;
+
 .autocomplete {
   position: relative;
 
@@ -21,10 +92,13 @@ export default {
   height: 100%;
 
   &__input {
+    position: relative;
+
     width: 100%;
     height: 100%;
 
     padding: 12px;
+    z-index: 2;
 
     border-radius: 4px;
     background: #ffffff;
@@ -40,14 +114,84 @@ export default {
   }
 
   &__options {
-    position: absolute;
-    top: 100%;
-    left: 0;
+    display: none;
 
-    width: 100%;
+    @include mx.desktop {
+      position: absolute;
+      top: 0;
+      left: 0;
+
+      display: flex;
+      flex-direction: column;
+      gap: 10px;
+
+      width: 100%;
+      height: auto;
+      max-height: 303px;
+
+      padding: 55px 20px 16px 20px;
+      opacity: 0;
+      visibility: 0;
+      z-index: -1;
+
+      overflow-y: auto;
+
+      width: 100%;
+
+      border-radius: 4px;
+      background: #ffffff;
+      border: 1px solid #e2e1f2;
+    }
+
+    &_active {
+      opacity: 1;
+      visibility: visible;
+      z-index: 1;
+
+      transition: all vars.$transition-duration ease-in-out;
+    }
   }
 
   &__options-item {
+    position: relative;
+
+    display: flex;
+    align-items: center;
+    gap: 5px;
+
+    width: 100%;
+
+    padding-bottom: 10px;
+    border-bottom: 1px solid #f3f3f3;
+
+    &:last-child {
+      border: none;
+      padding-bottom: 0;
+    }
+  }
+
+  &__options-item-icon {
+    width: 16px;
+    height: 16px;
+
+    color: #c2d2df;
+    fill: transparent;
+  }
+
+  &__options-item-text {
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 16px;
+
+    color: #898088;
+  }
+  &__options-item-link {
+    position: absolute;
+    top: 0;
+    left: 0;
+
+    width: 100%;
+    height: 100%;
   }
 }
 </style>
